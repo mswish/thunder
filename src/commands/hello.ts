679c20ca952210b22b9c45443cf940c7d98f9355
cylax00 <66838605+cylax00@users.noreply.github.com>
2020-06-13 05:04:26 -0400

    editing swish'

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,1 +1,22 @@
-a
+import { IBotCommand } from "../interfaces";
+import logger from "../utility/logger";
+import { Client, Message, MessageEmbed } from "discord.js";
+
+export default class clear implements IBotCommand {
+    public get name(): string { return this._name};
+
+    private _name: string = 'hello';
+
+    public async run(args, message): Promise<void> {
+        if(!args[1]) { 
+            message.channel.send(`Hello! I hope you have a wonderful day. Thanks for saying hello to me.`);
+            return;
+        };
+
+       
+    public getHelp(res: MessageEmbed): MessageEmbed {
+        res.addField('Hello: Say hello to thunder bot for fun!');
+        return res;
+    }
+
+}
